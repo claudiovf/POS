227222238f13572ca9049df334c56fbd8554dfed
Claudio <claudiovf@hotmail.com>
2020-06-10 01:02:04 +1000

    cart update, Total Update

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ let menuNew = document.querySelector('.menu-new');
 let mainCateg = document.querySelector('.main-categ')
 let mainItems = document.querySelector('.main-items');
 let categOption = document.querySelector('.main-categ');
+let mainPay = document.querySelector('.main-pay');
+let mainCart = document.querySelector('.main-cart');
+let orderItems = document.querySelector('.order-items');
+let orderItemList = document.querySelector('.order-items');
+let orderPrice = document.querySelector('.order-price');
+let orderTotal = document.querySelector('.order-total');
+let textTotal = document.querySelector('.total-text');
+
 
 function categDisplay(){
     mainCateg.classList.toggle('categ-active');
@@ -14,6 +22,12 @@ function categDisplay(){
     }else{
         menuNew.textContent = "NEW";
         menuNew.style.color = "rgb(20, 204, 158)";
+        mainPay.classList.remove('main-pay-active');
+        mainCart.classList.remove('main-cart-active');
+        mainItems.innerHTML = '';
+        orderItemList.innerHTML = '';
+        orderPrice.innerHTML = '';
+        orderTotal.innerHTML = '';
     }
 };
 
@@ -29,7 +43,12 @@ function showItems(event){
         });
         
     console.log(categItems);
-    
+
+    mainPay.classList.add('main-pay-active');
+    mainCart.classList.add('main-cart-active');
+ 
+
+    mainItems.innerHTML = '';
     for (let i = 0; i < categItems.length; i++) {
         let item = categItems[i];
         let itemButton = document.createElement('button');
@@ -37,12 +56,49 @@ function showItems(event){
         itemButton.appendChild(itemDisplay);
         mainItems.appendChild(itemButton);
     }
+
 };
 
 categOption.addEventListener('click', showItems);
 
 
 
+function addToOrder(event) {
+    let itemSel = event.target.innerHTML;
+    let itemText = "1 x " + itemSel;
+    let cartItem = document.createElement('div');
+    cartItem.textContent = itemText;
+    orderItemList.appendChild(cartItem);
+
+    console.log(itemSel, cartItem);
+
+    for (let i = 0; i < db.length; i++) {
+        let itemPrice = db[i].price;
+        let itemName = db[i].name;
+        
+        
+
+        // if(orderTotal.innerHTML == '') {
+        //     let totalSend = document.createElement('div')
+        //     totalSend.textContent = 0;
+        //     totalSend.classList.add('total-text');
+        //     orderTotal.appendChild(totalSend);
+
+        if(itemSel == itemName) {
+            let priceSend = document.createElement('div');
+            priceSend.textContent = itemPrice;
+            orderPrice.appendChild(priceSend);
+            
+            let currentTotal = orderTotal.firstChild.textContent;
+            
+            let newTotal = Number(textTotal.innerHTML) + Number(itemPrice);
+            textTotal.innerHTML = newTotal.toFixed(2);
+            console.log(currentTotal)
+        }
+    }
+}
+
+mainItems.addEventListener('click', addToOrder);
 
 // START OF ADD NEW PRODUCT FORM
 
