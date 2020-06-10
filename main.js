b5723ca840903dd0caedd3690e77a7991cd365e0
Claudio <claudiovf@hotmail.com>
2020-06-10 14:53:58 +1000

    main buttons void fix

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ let mainItems = document.querySelector('.main-items');
 let categOption = document.querySelector('.main-categ');
 let mainPay = document.querySelector('.main-pay');
 let mainCart = document.querySelector('.main-cart');
-let orderItems = document.querySelector('.order-items');
+let orderItemsWrap = document.querySelector('.order-items-wrap');
 let orderItemList = document.querySelector('.order-items');
 let orderPrice = document.querySelector('.order-price');
 let orderTotal = document.querySelector('.order-total');
 let textTotal = document.querySelector('.total-text');
-
+let mainAddon = document.querySelector('.main-addon');
 
 function categDisplay(){
     mainCateg.classList.toggle('categ-active');
@@ -24,8 +24,10 @@ function categDisplay(){
         menuNew.style.color = "rgb(20, 204, 158)";
         mainPay.classList.remove('main-pay-active');
         mainCart.classList.remove('main-cart-active');
+        mainAddon.classList.remove('addon-active');
+        mainAddon.innerHTML = '';
         mainItems.innerHTML = '';
-        orderItemList.innerHTML = '';
+        orderItemsWrap.innerHTML = '';
         orderPrice.innerHTML = '';
         textTotal.innerHTML = '0';
     }
@@ -52,6 +54,7 @@ function showItems(event){
     for (let i = 0; i < categItems.length; i++) {
         let item = categItems[i];
         let itemButton = document.createElement('button');
+        itemButton.classList.add('button-class');
         let itemDisplay = document.createTextNode(item.name);
         itemButton.appendChild(itemDisplay);
         mainItems.appendChild(itemButton);
@@ -65,36 +68,50 @@ categOption.addEventListener('click', showItems);
 
 function addToOrder(event) {
     let itemSel = event.target.innerHTML;
-    let itemText = "1 x " + itemSel;
-    let cartItem = document.createElement('div');
-    cartItem.textContent = itemText;
-    orderItemList.appendChild(cartItem);
+    console.log(itemSel, event.target.innerHTML.length);
 
-    console.log(itemSel, cartItem);
+    if(event.target.innerHTML.length < 40) {
 
-    for (let i = 0; i < db.length; i++) {
-        let itemPrice = db[i].price;
-        let itemName = db[i].name;
-        
-        
+        mainAddon.classList.add('addon-active');
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('order-items');
+        orderItemsWrap.appendChild(itemBox);
 
-        // if(orderTotal.innerHTML == '') {
-        //     let totalSend = document.createElement('div')
-        //     totalSend.textContent = 0;
-        //     totalSend.classList.add('total-text');
-        //     orderTotal.appendChild(totalSend);
 
-        if(itemSel == itemName) {
-            let priceSend = document.createElement('div');
-            priceSend.textContent = itemPrice;
-            orderPrice.appendChild(priceSend);
+        let itemText = "1 x " + itemSel;
+        let cartItem = document.createElement('div');
+        cartItem.textContent = itemText;
+        cartItem.classList.add('order-items-name');
+        itemBox.appendChild(cartItem);
+
+        console.log(itemSel, cartItem);
+
+        for (let i = 0; i < db.length; i++) {
+            let itemPrice = db[i].price;
+            let itemName = db[i].name;
             
-            let currentTotal = orderTotal.firstChild.textContent;
             
-            let newTotal = Number(textTotal.innerHTML) + Number(itemPrice);
-            textTotal.innerHTML = newTotal.toFixed(2);
-            console.log(currentTotal)
+
+            // if(orderTotal.innerHTML == '') {
+            //     let totalSend = document.createElement('div')
+            //     totalSend.textContent = 0;
+            //     totalSend.classList.add('total-text');
+            //     orderTotal.appendChild(totalSend);
+
+            if(itemSel == itemName) {
+                let priceSend = document.createElement('div');
+                priceSend.classList.add('order-price');
+                priceSend.textContent = itemPrice;
+                itemBox.appendChild(priceSend);
+                
+                let currentTotal = orderTotal.firstChild.textContent;
+                
+                let newTotal = Number(textTotal.innerHTML) + Number(itemPrice);
+                textTotal.innerHTML = newTotal.toFixed(2);
+                console.log(currentTotal)
+            }
         }
+        event.preventDefault();
     }
 }
 
