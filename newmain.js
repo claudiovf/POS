6d67380ec9e78f9116ce1bc9c9f7dea5035e8f1b
claudiovf <64691675+claudiovf@users.noreply.github.com>
2020-07-18 15:55:16 +1000

    Update newmain.js

--- newmain.js
+++ newmain.js
@@ -210,7 +210,7 @@ function addToOrder(event) {
     };
     
     let selectFrom = event.path[1].className;
-    console.log(event.path[1].className);
+    console.log(event.target.path[1].className);
         
     if( itemNameSel.length > 0 && itemNameSel.length < 60) {
         addToItemBox(itemNameSel, itemPrice(), selectFrom);
